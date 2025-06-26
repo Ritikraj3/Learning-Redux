cc4a8ad9b604443e60ce084f1a66d98f2ec0360a
Ritikraj3 <ritikraj5776@gmail.com>
2025-06-26 19:38:04 +0530

    mutatng & non-mutating

--- 01_Redux-Fundamentals/script.js
+++ 01_Redux-Fundamentals/script.js
@@ -1,3 +1,23 @@
 let state = {
-    count: 0
+    count: 0,
+    name: 'John',
+    age: 30
 }
+
+let prevState = state
+
+function increment() {
+
+    //mutating
+    // state.count = state.count + 1
+
+    // non-mutating
+    state = {...state, count:state.count + 1}
+}
+
+increment()
+console.log(state)
+increment()
+console.log(state)
+increment()
+console.log(state)
